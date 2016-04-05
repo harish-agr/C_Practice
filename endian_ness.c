4b2c966e2a697a1696d907cfd229d476cf5e5126
JagadeeshPagadala <jagadeesh.pgdl@gmail.com>
2016-04-05 22:43:09 +0530

    Written a proper logic to find Endian-ness of a Machine.

--- endian_ness.c
+++ endian_ness.c
@@ -1,12 +1,19 @@
+/* 
+	A simple program to find endian-ness of a Machine.
+	Tested on X86_64(LE), MPIS(BE)
+	Author: Jagadeesh Pagadala
+	Date: 05-04-2016
+*/
+	
 #include <stdio.h>
 #include <stddef.h>
 
 int main()
 {
-	int x = 0XABCD;
+	int x = 0X0A0B0C0D;
 	char *a = (char*)&x; 
 
-	if(a[0] == 0XD + '0') {
+	if(*a == 0XD ) {
 		printf("\nLittle Endian\n");
 	}else if (*a == 0XA) {
 		printf("\nBig Endian\n");
