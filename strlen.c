9905837d8b71f55d2d7f9c0667bc99fe77617475
Pagadala Jagadeesh <pagadala@blrrsa0011.blr.lantiq.com>
2016-04-11 15:49:02 +0530

    corrected sentence.

--- strlen.c
+++ strlen.c
@@ -24,7 +24,7 @@ int main()
 	char *my_name_is = "My Name is Jagadeesh"; /* This will be stored in 
 					Code segment of the program
 					Address is 0x400694 */
-	char MY_NAME[] = "My Name is Siva"; /* This will be stored in Data Segment 
+	char MY_NAME[] = "My Name is Siva"; /* This will be stored in Data Segment(stack) 
 					Address is 0x7fffffffdd20 */
 	
  	printf("\n strlen(%s) is: %d\n", my_name_is, str_len(my_name_is));	
