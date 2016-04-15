79a52b4b53e4b01afaa5920c909d86aafa2261cb
Pagadala Jagadeesh <pagadala@blrrsa0011.blr.lantiq.com>
2016-04-15 12:08:48 +0530

    usage of typeof operator.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-ALL= model cmp_enum_int  endian_ness  htonl  lsb8  offsetof  oom_killer_test  sizeof strlen strrev swap bitwise_ops char_overflow
+ALL= model cmp_enum_int  endian_ness  htonl  lsb8  offsetof  oom_killer_test  sizeof strlen strrev swap bitwise_ops char_overflow typeof
 
 all: $(ALL)
 
---
+++ typeof.c
@@ -0,0 +1,26 @@
+/*****************************************************************************
+*	Description	:	
+*	Author		:	Jagadeesh Pagadala
+*	Date		:	15-04-2016
+*
+*****************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	typedef struct {
+		int a;
+		int b;
+	}tmp;
+	int x = 10;
+	typeof(x) z;
+	typeof(tmp) t1 = {10,50};
+	tmp t2; 
+
+	z = 6;
+
+	printf("\n  sizeof(z):%d   \n", z);
+
+	return 0;
+}
